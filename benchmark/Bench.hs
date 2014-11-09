cfb29ef67e20bd74e1601ffd1842308c27abcccc
Greg V <greg@unrelenting.technology>
2014-11-09 21:43:24 +0300

    cleanup, release 0.4.1

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -2,14 +2,12 @@ module Main (main) where
 
 import           Criterion.Main (bgroup, defaultMain)
 import qualified GitsonBench
--- HASKELETON: import qualified New.ModuleBench
 
 main :: IO ()
 main = sequence_
     [ GitsonBench.setup
     ] >> defaultMain
     [ bgroup "Gitson" GitsonBench.benchmarks
-    -- HASKELETON: , bgroup "New.Module" New.ModuleBench.benchmarks
     ] >> sequence_
     [ GitsonBench.cleanup
     ]
