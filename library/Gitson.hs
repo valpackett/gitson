10c7e4f589e8f7dfe6916f14fe0a0167b3ee5188
Greg V <greg@unrelenting.technology>
2015-02-21 00:29:48 +0300

    use UnicodeSyntax

--- library/Gitson.hs
+++ library/Gitson.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Safe, FlexibleContexts #-}
+{-# LANGUAGE Safe, FlexibleContexts, UnicodeSyntax #-}
 
 -- | Gitson is a simple document store library for Git + JSON.
 module Gitson (
@@ -40,133 +40,133 @@ type TransactionWriter = WriterT [IO ()]
 
 type IdAndName = (Int, String)
 type FileName = String
-type Finder = [(IdAndName, FileName)] -> Maybe (IdAndName, FileName)
+type Finder = [(IdAndName, FileName)] → Maybe (IdAndName, FileName)
 
-splitFindDocument :: (MonadIO i) => FilePath -> Finder -> i (Maybe (IdAndName, FileName))
+splitFindDocument ∷ (MonadIO i) ⇒ FilePath → Finder → i (Maybe (IdAndName, FileName))
 splitFindDocument collection finder = listDocumentKeys collection >>=
-  return . finder . mapMaybe (\x -> intoFunctor (maybeReadIntString x) x)
+  return . finder . mapMaybe (\x → intoFunctor (maybeReadIntString x) x)
 
-documentFullKey :: (MonadIO i) => FilePath -> Finder -> i (Maybe FileName)
+documentFullKey ∷ (MonadIO i) ⇒ FilePath → Finder → i (Maybe FileName)
 documentFullKey collection finder = splitFindDocument collection finder >>= return . (snd <$>)
 
-findById :: Int -> Finder
+findById ∷ Int → Finder
 findById i = find $ (== i) . fst . fst
 
-findByName :: String -> Finder
+findByName ∷ String → Finder
 findByName n = find $ isSuffixOf n . snd . fst
 
 -- | Creates a git repository under a given path.
-createRepo :: FilePath -> IO ()
+createRepo ∷ FilePath → IO ()
 createRepo path = do
   createDirectoryIfMissing True path
   insideDirectory path $ shell "git" ["init"]
 
 -- | Executes a blocking transaction on a repository, committing the results to git.
-transaction :: (MonadIO i, MonadBaseControl IO i) => FilePath -> TransactionWriter i () -> i ()
+transaction ∷ (MonadIO i, MonadBaseControl IO i) ⇒ FilePath → TransactionWriter i () → i ()
 transaction repoPath action =
   insideDirectory repoPath $ do
     liftIO $ writeFile lockPath ""
     withLock lockPath Exclusive Block $ do
-      writeActions <- execWriterT action
+      writeActions ← execWriterT action
       shell "git" ["stash"] -- it's totally ok to do this without changes
       liftIO $ sequence_ writeActions
       shell "git" ["add", "--all"]
       shell "git" ["commit", "-m", "Gitson transaction"]
       shell "git" ["stash", "pop"]
 
-combineKey :: IdAndName -> FileName
+combineKey ∷ IdAndName → FileName
 combineKey (n, s) = zeroPad n ++ "-" ++ s
-  where zeroPad :: Int -> String
+  where zeroPad ∷ Int → String
         zeroPad = printf "%06d"
 
-writeDocument :: ToJSON a => FilePath -> FileName -> a -> IO ()
+writeDocument ∷ ToJSON a ⇒ FilePath → FileName → a → IO ()
 writeDocument collection key content = BL.writeFile (documentPath collection key) (encode content)
 
 -- | Adds a write action to a transaction.
-saveDocument :: (MonadIO i, ToJSON a) => FilePath -> FileName -> a -> TransactionWriter i ()
+saveDocument ∷ (MonadIO i, ToJSON a) ⇒ FilePath → FileName → a → TransactionWriter i ()
 saveDocument collection key content =
   tell [createDirectoryIfMissing True collection,
         writeDocument collection key content]
 
 -- | Adds a write action to a transaction.
 -- The key will start with a numeric id, incremented from the last id in the collection.
-saveNextDocument :: (MonadIO i, ToJSON a) => FilePath -> FileName -> a -> TransactionWriter i ()
+saveNextDocument ∷ (MonadIO i, ToJSON a) ⇒ FilePath → FileName → a → TransactionWriter i ()
 saveNextDocument collection key content =
   tell [createDirectoryIfMissing True collection,
         listDocumentKeys collection >>=
         return . nextKeyId >>=
-        \nextId -> writeDocument collection (combineKey (nextId, key)) content]
+        \nextId → writeDocument collection (combineKey (nextId, key)) content]
 
 -- | Adds a write action to a transaction.
 -- Will update the document with the given numeric id.
-saveDocumentById :: (MonadIO i, ToJSON a) => FilePath -> Int -> a -> TransactionWriter i ()
+saveDocumentById ∷ (MonadIO i, ToJSON a) ⇒ FilePath → Int → a → TransactionWriter i ()
 saveDocumentById collection i content =
   tell [documentFullKey collection (findById i) >>=
-        \k -> case k of
-          Just key -> writeDocument collection key content
-          Nothing -> return ()]
+        \k → case k of
+          Just key → writeDocument collection key content
+          Nothing → return ()]
 
 -- | Adds a write action to a transaction.
 -- Will update the document with the given numeric id.
-saveDocumentByName :: (MonadIO i, ToJSON a) => FilePath -> String -> a -> TransactionWriter i ()
+saveDocumentByName ∷ (MonadIO i, ToJSON a) ⇒ FilePath → String → a → TransactionWriter i ()
 saveDocumentByName collection n content =
   tell [documentFullKey collection (findByName n) >>=
-        \k -> case k of
-          Just key -> writeDocument collection key content
-          Nothing -> return ()]
+        \k → case k of
+          Just key → writeDocument collection key content
+          Nothing → return ()]
 
 -- | Lists collections in the current repository.
-listCollections :: (MonadIO i) => i [FilePath]
+listCollections ∷ (MonadIO i) ⇒ i [FilePath]
 listCollections = liftIO $ do
-  contents <- try (getDirectoryContents =<< getCurrentDirectory) :: IO (Either IOException [FilePath])
+  contents ← try (getDirectoryContents =<< getCurrentDirectory) ∷ IO (Either IOException [FilePath])
   filterDirs $ fromMaybe [] $ hush contents
 
 -- | Lists document keys in a collection.
-listDocumentKeys :: (MonadIO i) => FilePath -> i [FileName]
+listDocumentKeys ∷ (MonadIO i) ⇒ FilePath → i [FileName]
 listDocumentKeys collection = liftIO $ do
-  contents <- try (getDirectoryContents collection) :: IO (Either IOException [String])
+  contents ← try (getDirectoryContents collection) ∷ IO (Either IOException [String])
   return $ filterFilenamesAsKeys $ fromMaybe [] $ hush contents
 
 -- | Lists entries in a collection.
-listEntries :: (MonadIO i, FromJSON a) => FilePath -> i [a]
+listEntries ∷ (MonadIO i, FromJSON a) ⇒ FilePath → i [a]
 listEntries collection = liftIO $ do
-  ks <- listDocumentKeys collection
-  maybes <- mapM (readDocument collection) ks
+  ks ← listDocumentKeys collection
+  maybes ← mapM (readDocument collection) ks
   return $ fromMaybe [] $ sequence maybes
 
 -- | Reads a document from a collection by key.
-readDocument :: (MonadIO i, FromJSON a) => FilePath -> FileName -> i (Maybe a)
+readDocument ∷ (MonadIO i, FromJSON a) ⇒ FilePath → FileName → i (Maybe a)
 readDocument collection key = liftIO $ do
-  jsonString <- try (BL.readFile $ documentPath collection key) :: IO (Either IOException BL.ByteString)
+  jsonString ← try (BL.readFile $ documentPath collection key) ∷ IO (Either IOException BL.ByteString)
   return $ decode =<< hush jsonString
 
-readDocument' :: (MonadIO i, FromJSON a) => FilePath -> Maybe FileName -> i (Maybe a)
+readDocument' ∷ (MonadIO i, FromJSON a) ⇒ FilePath → Maybe FileName → i (Maybe a)
 readDocument' collection key = liftIO $ case key of
-  Just key -> readDocument collection key
-  Nothing -> return Nothing
+  Just key → readDocument collection key
+  Nothing → return Nothing
 
 -- | Reads a document from a collection by numeric id (for example, key "00001-hello" has id 1).
-readDocumentById :: (MonadIO i, FromJSON a) => FilePath -> Int -> i (Maybe a)
+readDocumentById ∷ (MonadIO i, FromJSON a) ⇒ FilePath → Int → i (Maybe a)
 readDocumentById collection i =
   documentFullKey collection (findById i) >>=
   readDocument' collection
 
 -- | Reads a document from a collection by name (for example, key "00001-hello" has name "hello").
-readDocumentByName :: (MonadIO i, FromJSON a) => FilePath -> String -> i (Maybe a)
+readDocumentByName ∷ (MonadIO i, FromJSON a) ⇒ FilePath → String → i (Maybe a)
 readDocumentByName collection n =
   documentFullKey collection (findByName n) >>=
   readDocument' collection
 
 -- | Returns a document's id by name (for example, "hello" will return 23 when key "00023-hello" exists).
 -- Does not read the document!
-documentIdFromName :: (MonadIO i) => FilePath -> String -> i (Maybe Int)
+documentIdFromName ∷ (MonadIO i) ⇒ FilePath → String → i (Maybe Int)
 documentIdFromName collection n =
   splitFindDocument collection (findByName n) >>=
   return . (fst <$> fst <$>)
 
 -- | Returns a document's name by id (for example, 23 will return "hello" when key "00023-hello" exists).
 -- Does not read the document!
-documentNameFromId :: (MonadIO i) => FilePath -> Int -> i (Maybe String)
+documentNameFromId ∷ (MonadIO i) ⇒ FilePath → Int → i (Maybe String)
 documentNameFromId collection i =
   splitFindDocument collection (findById i) >>=
   return . (drop 1 . snd <$> fst <$>)
--- test-suite/GitsonSpec.hs
+++ test-suite/GitsonSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, UnicodeSyntax #-}
 
 module GitsonSpec (spec) where
 
@@ -12,12 +12,12 @@ import           Control.Monad (void)
 import           Gitson
 import           Gitson.Util (insideDirectory, lastCommitText)
 
-{-# ANN module ("HLint: ignore Redundant do"::String) #-}
+{-# ANN module ("HLint: ignore Redundant do" :: String) #-}
 
-data Thing = Thing { val :: Int } deriving (Eq, Show, Ord)
+data Thing = Thing { val ∷ Int } deriving (Eq, Show, Ord)
 $(deriveJSON defaultOptions ''Thing)
 
-spec :: Spec
+spec ∷ Spec
 spec = before setup $ after cleanup $ do
   context "transaction" $ do
     describe "saveDocument" $ do
@@ -28,11 +28,11 @@ spec = before setup $ after cleanup $ do
           liftIO $ readFile "things/first-thing.json" `shouldThrow` anyIOException
           liftIO $ readFile "things/second-thing.json" `shouldThrow` anyIOException
         insideDirectory "tmp/repo" $ do
-          first <- readFile "things/first-thing.json"
+          first ← readFile "things/first-thing.json"
           first `shouldBe` "{\n  \"val\": 1\n}"
-          second <- readFile "things/second-thing.json"
+          second ← readFile "things/second-thing.json"
           second `shouldBe` "{\n  \"val\": 2\n}"
-          commitMsg <- lastCommitText
+          commitMsg ← lastCommitText
           commitMsg `shouldBe` "Gitson transaction"
 
     describe "saveNextDocument" $ do
@@ -41,108 +41,108 @@ spec = before setup $ after cleanup $ do
           saveNextDocument "things" "hello" Thing {val = 1}
           saveNextDocument "things" "world" Thing {val = 2}
         insideDirectory "tmp/repo" $ do
-          first <- readFile "things/000001-hello.json"
+          first ← readFile "things/000001-hello.json"
           first `shouldBe` "{\n  \"val\": 1\n}"
-          second <- readFile "things/000002-world.json"
+          second ← readFile "things/000002-world.json"
           second `shouldBe` "{\n  \"val\": 2\n}"
 
     describe "saveDocumentById" $ do
       it "updates an entry by specified id" $ do
         createDirectoryIfMissing True "tmp/repo/things"
-        _ <- liftIO $ writeFile "tmp/repo/things/000004-second-thing.json" "{\"val\":1}"
+        _ ← liftIO $ writeFile "tmp/repo/things/000004-second-thing.json" "{\"val\":1}"
         transaction "tmp/repo" $ do
           saveDocumentById "things" 4 Thing {val = 2}
         insideDirectory "tmp/repo" $ do
-          second <- readFile "things/000004-second-thing.json"
+          second ← readFile "things/000004-second-thing.json"
           second `shouldBe` "{\n  \"val\": 2\n}"
 
     describe "saveDocumentByName" $ do
       it "updates an entry by specified name" $ do
         createDirectoryIfMissing True "tmp/repo/things"
-        _ <- liftIO $ writeFile "tmp/repo/things/000004-second-thing.json" "{\"val\":1}"
+        _ ← liftIO $ writeFile "tmp/repo/things/000004-second-thing.json" "{\"val\":1}"
         transaction "tmp/repo" $ do
           saveDocumentByName "things" "second-thing" Thing {val = 2}
         insideDirectory "tmp/repo" $ do
-          second <- readFile "things/000004-second-thing.json"
+          second ← readFile "things/000004-second-thing.json"
           second `shouldBe` "{\n  \"val\": 2\n}"
 
   describe "readDocument" $ do
     it "returns Just the document when reading an document by key" $ do
       createDirectoryIfMissing True "tmp/repo/things"
-      _ <- writeFile "tmp/repo/things/second-thing.json" "{\"val\":2}"
-      content <- readDocument "tmp/repo/things" "second-thing" :: IO (Maybe Thing)
+      _ ← writeFile "tmp/repo/things/second-thing.json" "{\"val\":2}"
+      content ← readDocument "tmp/repo/things" "second-thing" ∷ IO (Maybe Thing)
       content `shouldBe` Just Thing {val = 2}
 
     it "returns Nothing when reading by a nonexistent key" $ do
-      content <- readDocument "tmp/repo/things" "totally-not-a-thing" :: IO (Maybe Thing)
+      content ← readDocument "tmp/repo/things" "totally-not-a-thing" ∷ IO (Maybe Thing)
       content `shouldBe` Nothing
 
   describe "readDocumentById" $ do
     it "returns Just the document when reading an document by id" $ do
       createDirectoryIfMissing True "tmp/repo/things"
-      _ <- writeFile "tmp/repo/things/000004-second-thing.json" "{\"val\":1}"
-      content <- readDocumentById "tmp/repo/things" 4 :: IO (Maybe Thing)
+      _ ← writeFile "tmp/repo/things/000004-second-thing.json" "{\"val\":1}"
+      content ← readDocumentById "tmp/repo/things" 4 ∷ IO (Maybe Thing)
       content `shouldBe` Just Thing {val = 1}
 
     it "returns Nothing when reading by a nonexistent id" $ do
-      content <- readDocumentById "tmp/repo/things" 1 :: IO (Maybe Thing)
+      content ← readDocumentById "tmp/repo/things" 1 ∷ IO (Maybe Thing)
       content `shouldBe` Nothing
 
   describe "readDocumentByName" $ do
     it "returns Just the document when reading an document by name" $ do
       createDirectoryIfMissing True "tmp/repo/things"
-      _ <- writeFile "tmp/repo/things/000098-second-thing.json" "{\"val\":1}"
-      content <- readDocumentByName "tmp/repo/things" "second-thing" :: IO (Maybe Thing)
+      _ ← writeFile "tmp/repo/things/000098-second-thing.json" "{\"val\":1}"
+      content ← readDocumentByName "tmp/repo/things" "second-thing" ∷ IO (Maybe Thing)
       content `shouldBe` Just Thing {val = 1}
 
     it "returns Nothing when reading by a nonexistent name" $ do
-      content <- readDocumentByName "tmp/repo/things" "yolo" :: IO (Maybe Thing)
+      content ← readDocumentByName "tmp/repo/things" "yolo" ∷ IO (Maybe Thing)
       content `shouldBe` Nothing
 
   describe "documentIdFromName" $ do
     it "returns Just the document id from the document's name" $ do
       createDirectoryIfMissing True "tmp/repo/things"
-      _ <- writeFile "tmp/repo/things/000098-some-thing.json" "THIS SHOULD NOT BE READ"
-      i <- documentIdFromName "tmp/repo/things" "some-thing"
+      _ ← writeFile "tmp/repo/things/000098-some-thing.json" "THIS SHOULD NOT BE READ"
+      i ← documentIdFromName "tmp/repo/things" "some-thing"
       i `shouldBe` Just 98
 
     it "returns Nothing when run with a nonexistent name" $ do
-      i <- documentIdFromName "tmp/repo/things" "yolo"
+      i ← documentIdFromName "tmp/repo/things" "yolo"
       i `shouldBe` Nothing
 
   describe "documentNameFromId" $ do
     it "returns Just the document name from the document's id" $ do
       createDirectoryIfMissing True "tmp/repo/things"
-      _ <- writeFile "tmp/repo/things/000098-some-thing.json" "THIS SHOULD NOT BE READ"
-      i <- documentNameFromId "tmp/repo/things" 98
+      _ ← writeFile "tmp/repo/things/000098-some-thing.json" "THIS SHOULD NOT BE READ"
+      i ← documentNameFromId "tmp/repo/things" 98
       i `shouldBe` Just "some-thing"
 
     it "returns Nothing when run with a nonexistent id" $ do
-      i <- documentNameFromId "tmp/repo/things" 123
+      i ← documentNameFromId "tmp/repo/things" 123
       i `shouldBe` Nothing
 
   describe "listDocumentKeys" $ do
     it "returns a list of document keys when listing a collection" $ do
       createDirectoryIfMissing True "tmp/repo/things"
-      _ <- writeFile "tmp/repo/things/first-thing.json" "{}"
-      _ <- writeFile "tmp/repo/things/second-thing.json" "{}"
-      list <- listDocumentKeys "tmp/repo/things"
+      _ ← writeFile "tmp/repo/things/first-thing.json" "{}"
+      _ ← writeFile "tmp/repo/things/second-thing.json" "{}"
+      list ← listDocumentKeys "tmp/repo/things"
       sort list `shouldBe` ["first-thing", "second-thing"]
 
     it "returns an empty when listing a nonexistent collection" $ do
-      list <- listDocumentKeys "nonsense"
+      list ← listDocumentKeys "nonsense"
       list `shouldBe` []
 
   describe "listEntries" $ do
     it "returns a list of entries when listing a collection" $ do
       createDirectoryIfMissing True "tmp/repo/things"
-      _ <- writeFile "tmp/repo/things/first-thing.json" "{\"val\":1}"
-      _ <- writeFile "tmp/repo/things/second-thing.json" "{\"val\":2}"
-      list <- listEntries "tmp/repo/things" :: IO [Thing]
+      _ ← writeFile "tmp/repo/things/first-thing.json" "{\"val\":1}"
+      _ ← writeFile "tmp/repo/things/second-thing.json" "{\"val\":2}"
+      list ← listEntries "tmp/repo/things" ∷ IO [Thing]
       sort list `shouldBe` [Thing {val = 1}, Thing {val = 2}]
 
     it "returns an empty list when listing a nonexistent collection" $ do
-      list <- listEntries "nonsense" :: IO [Thing]
+      list ← listEntries "nonsense" ∷ IO [Thing]
       list `shouldBe` []
 
   describe "listCollections" $ do
@@ -150,17 +150,17 @@ spec = before setup $ after cleanup $ do
       createDirectoryIfMissing True "tmp/repo/bits"
       createDirectoryIfMissing True "tmp/repo/pieces"
       insideDirectory "tmp/repo" $ do
-        list <- listCollections
+        list ← listCollections
         sort list `shouldBe` ["bits", "pieces"]
 
     it "returns an empty list when listing an empty repo" $ do
       createDirectoryIfMissing True "tmp/repo"
       insideDirectory "tmp/repo" $ do
-        list <- listCollections
+        list ← listCollections
         sort list `shouldBe` []
 
-setup :: IO ()
+setup ∷ IO ()
 setup = createRepo "tmp/repo"
 
-cleanup :: ActionWith ()
-cleanup () = void (try (removeDirectoryRecursive "tmp/repo") :: IO (Either IOException ()))
+cleanup ∷ ActionWith ()
+cleanup () = void (try (removeDirectoryRecursive "tmp/repo") ∷ IO (Either IOException ()))
